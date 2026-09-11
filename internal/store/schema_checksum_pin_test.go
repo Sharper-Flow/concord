f65eb3e1e407658f9913df146ee07cd62691da87
JRedeker <jon.redeker@gmail.com>
2026-09-10 22:41:31 -0400

    test(store): pin migration 79 checksum

--- internal/store/schema_checksum_pin_test.go
+++ internal/store/schema_checksum_pin_test.go
@@ -92,6 +92,7 @@ var migrationChecksumPins = map[int]string{
 	76: "84673ba8ad751a5e0d80b2c0101aadf139bfb6028abfc45f3c54c7db1bd29e04",
 	77: "57e0d66468bc2ec0aed6eed89e81b891e5d4d8a682f8d18dc09165ce90f0eb8f",
 	78: "ffaf1335d53d51fb8dbc8f5ef111d86ac62848e1dc0b58d44acad8458d5f9d63",
+	79: "329b005809086dbb90769b7ccb5d792695bb0e117ba401850e56b0884586329c",
 }
 
 func TestMigrationChecksumPinsMatchDefinitions(t *testing.T) {
